258619b51dfe1ef5d4ea1da50067d8ad7d2585d8
JWMoebius <physicist.moebius@gmail.com>
2015-06-26 13:52:40 +0200

    Created Sphere.hpp and Sphere.cpp

---
+++ framework/Sphere.cpp
@@ -0,0 +1,17 @@
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+#include "Sphere.hpp"
+
+
+TEST_CASE("LIST1","[aufgabe1]")
+{
+	int i = 3;
+	List<int> thost;
+	REQUIRE(i==3);
+	REQUIRE(thost.size()==0);
+}
+
+int main(int argc, char* argv [])
+{
+	return Catch::Session().run(argc, argv);
+}
---
+++ framework/Sphere.hpp
@@ -0,0 +1,16 @@
+#ifndef BUW_LIST_HPP
+#define BUW_LIST_HPP
+
+class Sphere{
+	virtual int area()=0;
+	virtual int volume()=0;
+};
+	
+	
+
+
+
+
+
+
+#endif //#define BUW_LIST_HPP
