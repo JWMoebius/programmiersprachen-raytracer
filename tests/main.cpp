03f17a59a747a241978bc3ebe36fb7e0208541f4
JWMoebius <physicist.moebius@gmail.com>
2015-06-26 22:07:02 +0200

    Test for area of a Spehere implemented

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,14 @@
 #define CATCH_CONFIG_RUNNER
-#include <catch.hpp>
+#include "../framework/Shape.hpp"
+#include "../framework/catch.hpp"
+
+TEST_CASE("AREA","[Aufgabe2area]")
+{
+	Sphere m(7, glm::vec3(0));
+	float m_area =m.area();
+	REQUIRE(m_area==7);
+}
+
 
 int main(int argc, char *argv[])
 {
